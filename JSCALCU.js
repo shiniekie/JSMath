254f3e4595f1c9eb36f072ca926eb7b67f2648b3
Shinie Kie Avila-Dangasi <shiniekie.dangasi@nmsc.edu.ph>
2026-03-07 21:48:34 +0800

    Add files via upload

--- JSCALCU.js
+++ JSCALCU.js
@@ -1,5 +1,3 @@
-document.getElementById("solveBtn").addEventListener("click", calculate);
-
 function calculate() {
     var n1 = parseFloat(document.getElementById("num1").value);
     var n2 = parseFloat(document.getElementById("num2").value);
